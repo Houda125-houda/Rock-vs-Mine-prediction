b89b4d583820cbf0576bd316342e4364271acea8
Houda125-houda <63107186+Houda125-houda@users.noreply.github.com>
2021-09-20 21:04:16 +0100

    Rock vs Mine Prediction

---
+++ Sonar Rock vs Mine.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 20:08:55 2021
+
+@author: LENOVO
+"""
+
+# Submarine have to know if the enemy country have planted some mines " explosives that explodes when some object comes in contact with it " in the ocean
+# So the submarine need to predict whether it is crossing a mine or a rock 
+# so Sonar  sends sound signals and review switchbacks " le fait de revenir " so this signal is then processed to detect whether the object is just a mine or a rock in the ocean 
+# the sonar send and receive signals  bounce back " be returned" from metal cylindre and some rocks because mines will be made of metals
+# we will use logistic regression model because it works very well for binary classification problem 
